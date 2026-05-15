bf9c91d774f1533df1149e653b1b7bb0eb765eaf
Andrew Angelle <andrewangelle@gmail.com>
2026-05-15 15:53:49 -0400

    fix: auth protected routes and styles (#8)
    * fix: auth protected routes
    
    * fix: nav bar styles

--- src/routes/auth.sign-in.tsx
+++ src/routes/auth.sign-in.tsx
@@ -1,9 +1,19 @@
 import { Show, SignIn, UserButton } from '@clerk/tanstack-react-start';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import { fetchUserId } from '~/auth/middleware';
 import { NavBar } from '~/components/NavBar';
 import { FlexCenter } from '~/styles/Page';
 
 export const Route = createFileRoute('/auth/sign-in')({
+  async beforeLoad() {
+    const { userId } = await fetchUserId();
+    return { userId };
+  },
+  loader({ context }) {
+    if (context.userId) {
+      throw redirect({ to: '/boards' });
+    }
+  },
   component() {
     return (
       <>
