26e1c67a4552aac8163c54e98eb4fbbdae4fe3ac
Andrew Angelle <andrewangelle@gmail.com>
2026-06-07 19:54:33 -0400

    refactor: simplify hash checklist id hooks

--- src/utils/useHashChecklistId.ts
+++ src/utils/useHashChecklistId.ts
@@ -1,30 +1,11 @@
 import { useLocation } from '@tanstack/react-router';
 
+const CHECKLIST_HASH_PREFIX = 'checklist-';
+
 export function useHashChecklistId() {
-  const { hash: routerHash } = useLocation();
-  const CHECKLIST_HASH_PREFIX = 'checklist-';
+  const { hash } = useLocation();
 
-  function getScrollToChecklistId(hash: string) {
-    const normalizedHash = hash.startsWith('#') ? hash.slice(1) : hash;
-
-    if (!normalizedHash.startsWith(CHECKLIST_HASH_PREFIX)) {
-      return undefined;
-    }
-
-    return normalizedHash.slice(CHECKLIST_HASH_PREFIX.length);
-  }
-
-  function getChecklistHashFromLocation(routerHash: string) {
-    if (routerHash) {
-      return routerHash;
-    }
-
-    if (typeof window === 'undefined') {
-      return '';
-    }
-
-    return window.location.hash.slice(1);
-  }
-
-  return getScrollToChecklistId(getChecklistHashFromLocation(routerHash));
+  return hash.startsWith(CHECKLIST_HASH_PREFIX)
+    ? hash.slice(CHECKLIST_HASH_PREFIX.length)
+    : undefined;
 }
