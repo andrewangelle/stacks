b5f9a304ad8a5dca8755915d6c47d7f79774d91a
Andrew Angelle <andrewangelle@gmail.com>
2026-06-07 19:20:28 -0400

    feat: card title details checklist show more

--- src/components/Cards/CardTitleDetails/CardTitleDetailsChecklists.tsx
+++ src/components/Cards/CardTitleDetails/CardTitleDetailsChecklists.tsx
@@ -74,6 +74,7 @@ export function CardTitleDetailsChecklists({
             checklistViews?.singleChecklistId && (
               <CardTitleDetailsChecklist
                 checklistId={checklistViews?.singleChecklistId}
+                collapsible
               />
             )}
         </>
