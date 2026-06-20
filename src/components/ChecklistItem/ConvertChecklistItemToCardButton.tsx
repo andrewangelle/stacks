143c3cbd570df5dd03d96f8772ab95b847093c88
Andrew Angelle <andrewangelle@gmail.com>
2026-06-20 18:44:53 -0400

    refactor: split off convert item to card button

---
+++ src/components/ChecklistItem/ConvertChecklistItemToCardButton.tsx
@@ -0,0 +1,63 @@
+import { useEffect } from 'react';
+import { ChecklistItemOptionsListItem } from '~/components/ChecklistItem/ChecklistItem.styled';
+import { useCreateActivity } from '~/db/activity/activity.query';
+import { useCreateCard } from '~/db/cards/cards.query';
+import {
+  useDeleteChecklistItem,
+  useGetChecklistItem,
+} from '~/db/checklistItems/checklistItems.query';
+import { useCurrentBoardId } from '~/utils/useCurrentBoardId';
+
+export function ConvertChecklistItemToCardButton({ id }: { id: string }) {
+  const { data: checklistItem } = useGetChecklistItem({ itemId: id });
+  const boardId = useCurrentBoardId();
+  const createActivity = useCreateActivity();
+  const deleteChecklistItem = useDeleteChecklistItem();
+  const {
+    mutate: createCard,
+    isSuccess: isCardCreated,
+    data: response,
+  } = useCreateCard();
+
+  function convertToCard() {
+    if (checklistItem) {
+      createCard({
+        listId: checklistItem.listId,
+        cardTitle: checklistItem.label,
+      });
+    }
+  }
+
+  useEffect(() => {
+    if (isCardCreated && response.data[0].id && checklistItem) {
+      createActivity({
+        cardId: response.data[0].id,
+        listId: checklistItem.listId,
+        boardId,
+        type: 'feed',
+        content: `converted this card from a checklist item on {{ linkTo:${checklistItem.cardId} }}`,
+      });
+
+      deleteChecklistItem({
+        itemId: id,
+      });
+    }
+  }, [
+    isCardCreated,
+    response,
+    checklistItem,
+    boardId,
+    createActivity,
+    deleteChecklistItem,
+    id,
+  ]);
+
+  return (
+    <ChecklistItemOptionsListItem
+      data-testid="ConvertChecklistItemToCardButton"
+      onClick={convertToCard}
+    >
+      Convert to card
+    </ChecklistItemOptionsListItem>
+  );
+}
