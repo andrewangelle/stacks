a2032a0b71e7b8d18266e104fa99325e460a4538
Andrew Angelle <andrewangelle@gmail.com>
2026-09-04 09:48:33 -0400

    test: move factory to one file

--- tests/factories/card.ts
+++
@@ -1,8 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-export function buildCard(title?: string) {
-  return {
-    cardTitle: title ?? faker.hacker.phrase(),
-    cardDescription: faker.lorem.sentence(),
-  };
-}
--- tests/fixtures/seedBoard.ts
+++ tests/fixtures/seedBoard.ts
@@ -1,6 +1,6 @@
 import type { BoardBackground } from '~/components/Boards/Boards.styled';
 import { prisma } from '~/db/prisma';
-import { buildBoard } from '~test/factories/board';
+import { buildBoard } from '~test/factories';
 import { TEST_USER_ID } from '~test/mocks/constants';
 
 export async function seedBoard(data?: {
